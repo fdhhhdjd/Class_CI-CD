613793cbff88160627ae03adf74aef5191377616
fdhhhdjd <nguyentientai10@gmail.com>
2024-05-10 14:48:37 +0700

    fix: .env

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT
 server.get("/ci-cd", async (_, res, ___) => {
   res.status(200).json({
     status: 200,
-    message:"CI-CD Ok 🤖"
+    message:"CI-CD By Tai Ok 🤖"
   })
 });
 
